3afaf597c44dff65cbfe83592e4f0d60ca99c6a2
busti <busti@LAPTOP-DLR6RBB3>
2018-03-20 19:19:16 -0600

    Mis datos personales

---
+++ src/datos/Datos.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datos;
+
+/**
+ *
+ * @author busti
+ */
+public class Datos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre="Walter Antoniell Bustillo Ayala";
+        String fecha="14 de Marzo de 1999";
+        String carrera="Ingeniería Informática";
+        int edad=19;
+        System.out.println(" ----------------------");
+        System.out.println("| Mis Datos personales |");
+        System.out.println(" ----------------------");
+        System.out.println("Mi nombre completo es: " + nombre);
+        System.out.println("Mi fecha de nacimiento es: " + fecha);
+        System.out.println("Mi edad es: " + edad + " años");
+        System.out.println("La carrera que estudio es: " + carrera);
+        
+    }
+    
+}
